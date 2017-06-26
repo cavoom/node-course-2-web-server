7cd548dd59b5c9f75b0ce0051dd358546af3dec4
DavidHaas <cavoom@me.com>
2017-06-26 09:13:40 -0400

    change to server2

--- server.js
+++ server.js
@@ -1,94 +1,70 @@
+// Setup the server
 const express = require('express'); // require the Express Server module
-const hbs = require('hbs'); // This is the view engine that we loaded from npm
-const fs = require('fs'); // file system capabilities
 const port = process.env.PORT || 3000; // Use Heroku port or local port 3000
-
-var app = express(); // Make a new app
-
-hbs.registerPartials(__dirname + '/views/partials/');
-
-app.set('view engine', 'hbs'); // we need to configure hbs as our view engine
-
-// This is the easier way to setup pages to serve:
-// One command opens a directory that you can type the file names after the port
-// And it serves up the pages if they live in that directory
+var app = express();
 app.use(express.static(__dirname + '/public')); // this middleware sets up the server for us!\
 
-// app.use is how you setup middleware
-app.use((req, res, next) =>{
 
-    var now = new Date().toString();
-    var log = `${now}:${req.method} ${req.url}`;
+var getKairos = (callback) => {
 
-    console.log(log);
-    fs.appendFile('server.log',log + '\n', (err)=>{
-    if (err) {
-        console.log('Unable to append to server.log')
-        }
-    });
-    next(); // need to call next or program will hang here
-});
+// *** KAIROS See if the uploaded photo matches anything in the Kairos Gallery
+var Kairos = require('kairos-api'); // Load this in from NPM
+var client = new Kairos('89b85235', '7afdc8672469bc634354e561d61f7491'); // My user ID and Key
 
-// app.use((req, res, next) =>{
-//     res.render('maintenance.hbs');
+var results = {};
+var params = {
+//app_id: '89b85235',
+//app_key: '7afdc8672469bc634354e561d61f7491',
+gallery_name: 'gallerytest2',
+image: 'http://www.cavoom.com/file-upload/uploads/temp.jpg'
+
+};
+
+client.recognize(params) // Detect if there are faces in the photo
+.then(function(result) // When the result comes back, perform some logic
+    {
+        //console.log({result});
+        if(!!result.body.images[0].transaction.confidence){
+            
+            results = {result};
+            console.log('Confidence level: ', (result.body.images[0].transaction.confidence * 100).toFixed(1),'%');
+            //console.log(result.body.images[0].transaction)
+            //console.log(JSON.stringify(result, undefined, 2))
+                callback(result)
+                } else {
+                    console.log(JSON.stringify(result, undefined, 2))
+                    callback(result)
+                }
+                
+    }
     
-//     });
+    )
 
+.catch(function(err) { // Catch problems with this code
+console.log(JSON.stringify(err, undefined, 2))
 
-hbs.registerHelper('getCurrentYear', ()=>{
-return new Date().getFullYear()
-//return('stuff');
 });
+// ********** END KAIROS  ***********  
 
-hbs.registerHelper('screamIt', (text) =>{
-    return text.toUpperCase();
-})
+};
 
-
+// Now call getKairos Results and spin up the server
+getKairos ((theBigResult)=> {
+console.log('spinning up the servers');
+var stuff = theBigResult.body.images[0].transaction;
+var subject = theBigResult.body.images[0].transaction.subject_id;
 // New Home Page with Template
 app.get('/', (req, res) => {
-    //res.send('<h2>The About Page</h2>');
-    res.render('home.hbs', {
-        pageTitle: 'Home Page',
-        welcomeMessage: 'Hidalay Ho Neighborino',
-        //currentYear: new Date().getFullYear()
+    res.send(subject);
+    // Must wrap {stuff} in curly brackets when sending for print to web??
+    // Not sure why
 
-    });
+   });
     
-});
 
-// New Projects Page
-app.get('/projects', (req, res) => {
-    res.render('home.hbs', {
-        pageTitle: 'Projects Page',
-        welcomeMessage: 'This is your Projects Page',
-
-
-    });
-    
-});
-
-app.get('/about', (req, res) => {
-    //res.send('<h2>The About Page</h2>');
-    res.render('about.hbs', {
-        pageTitle: 'A Boot Page',
-        //currentYear: new Date().getFullYear()
-
-    });
-    
-});
-
-app.get('/bad', (req, res) => { // so when we get 
-    res.send({
-        message: 'Error 404 Bad Route Detected, yo!',
-        body: 'You typed in a bad page link. Try again, k'
-    });
-    
-});
-
-
+// Start Listening
 app.listen(port,()=>{
     console.log('The server is now up and running on port: ',port);
 }); // Port 3000 is a good port for local development - starts the listeer on port 3000
 
-
+})
---
+++ server1.js
@@ -0,0 +1,94 @@
+const express = require('express'); // require the Express Server module
+const hbs = require('hbs'); // This is the view engine that we loaded from npm
+const fs = require('fs'); // file system capabilities
+const port = process.env.PORT || 3000; // Use Heroku port or local port 3000
+
+var app = express(); // Make a new app
+
+hbs.registerPartials(__dirname + '/views/partials/');
+
+app.set('view engine', 'hbs'); // we need to configure hbs as our view engine
+
+// This is the easier way to setup pages to serve:
+// One command opens a directory that you can type the file names after the port
+// And it serves up the pages if they live in that directory
+app.use(express.static(__dirname + '/public')); // this middleware sets up the server for us!\
+
+// app.use is how you setup middleware
+app.use((req, res, next) =>{
+
+    var now = new Date().toString();
+    var log = `${now}:${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log',log + '\n', (err)=>{
+    if (err) {
+        console.log('Unable to append to server.log')
+        }
+    });
+    next(); // need to call next or program will hang here
+});
+
+// app.use((req, res, next) =>{
+//     res.render('maintenance.hbs');
+    
+//     });
+
+
+hbs.registerHelper('getCurrentYear', ()=>{
+return new Date().getFullYear()
+//return('stuff');
+});
+
+hbs.registerHelper('screamIt', (text) =>{
+    return text.toUpperCase();
+})
+
+
+// New Home Page with Template
+app.get('/', (req, res) => {
+    //res.send('<h2>The About Page</h2>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Hidalay Ho Neighborino',
+        //currentYear: new Date().getFullYear()
+
+    });
+    
+});
+
+// New Projects Page
+app.get('/projects', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Projects Page',
+        welcomeMessage: 'This is your Projects Page',
+
+
+    });
+    
+});
+
+app.get('/about', (req, res) => {
+    //res.send('<h2>The About Page</h2>');
+    res.render('about.hbs', {
+        pageTitle: 'A Boot Page',
+        //currentYear: new Date().getFullYear()
+
+    });
+    
+});
+
+app.get('/bad', (req, res) => { // so when we get 
+    res.send({
+        message: 'Error 404 Bad Route Detected, yo!',
+        body: 'You typed in a bad page link. Try again, k'
+    });
+    
+});
+
+
+app.listen(port,()=>{
+    console.log('The server is now up and running on port: ',port);
+}); // Port 3000 is a good port for local development - starts the listeer on port 3000
+
+
